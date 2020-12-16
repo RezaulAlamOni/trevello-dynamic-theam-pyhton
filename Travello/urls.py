b6232f363875f3b9ff0c6a703aa18fe5350d6cc3
RezaulAlamOni <rzalam93@gmail.com>
2020-12-17 00:42:13 +0600

    template jinja created

--- Travello/urls.py
+++ Travello/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('about',views.about, name='about'),
     path('blog',views.blogs, name='blogs'),
     path('single-blog',views.single_blog, name='single-blog'),
+    path('contact',views.contact, name='contact'),
+    path('travel_destination',views.travel_destination, name='travel_destination'),
+    path('destination_details',views.destination_details, name='destination_details'),
 ];
--- Travello/views.py
+++ Travello/views.py
@@ -9,12 +9,19 @@ def index(request):
 def about(request):
     return render(request, 'about.html');
 
-
 def blogs(request):
     return render(request, 'blog.html');
 
-
 def single_blog(request):
-    return render(request, 'about.html');
+    return render(request, 'single-blog.html');
+
+def contact(request):
+    return render(request, 'contact.html');
+
+def travel_destination(request):
+    return render(request, 'travel_destination.html');
+
+def destination_details(request):
+    return render(request, 'destination_details.html');
 
 
