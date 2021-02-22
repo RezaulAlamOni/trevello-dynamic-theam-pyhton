8cd71faec0609f41cc124a75e2423cd3b3559045
RezaulAlamOni <rzalam93@gmail.com>
2021-02-23 02:30:55 +0600

    Update code for user

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 
 # Create your views here.
@@ -11,5 +12,22 @@ def register(request):
         email = request.POST['email']
         pwd = request.POST['pwd']
         pwd2 = request.POST['pwd2']
+
+        if pwd == pwd2:
+            if User.objects.filter(username=username).exists():
+                print('User already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                print('Email already taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, first_name=f_name, last_name=l_name, email=email,                               password=pwd)
+                user.save()
+                print('User Create user')
+        else:
+            print('Password not match')
+            return redirect('register')
+
+        return redirect('/');
     else:
         return render(request, 'accounts/register.html')
